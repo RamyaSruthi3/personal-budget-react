1b5422fb4a6feeec1e5cfbc7c64d62c0f3c1d0ee
Ramya <rreddy11@uncc.edu>
2023-10-01 19:20:57 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function Hero() {
+  return (
+    <header>
+      <h1>Personal Budget</h1>
+      <h2>An efficient personal-budget management app</h2>
+    </header>
+  );
+}
+
+export default Hero;
